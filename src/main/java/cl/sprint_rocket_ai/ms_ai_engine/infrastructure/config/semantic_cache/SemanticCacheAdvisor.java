8e12a7a43e84f3ed2978b98dfb0cb11b2c67769a
scarreno-vrgroup_bicevida <scarreno.vrgroup@bicevida.cl>
2026-06-22 18:51:40 -0400

    feat: fix

--- src/main/java/cl/sprint_rocket_ai/ms_ai_engine/infrastructure/config/semantic_cache/SemanticCacheAdvisor.java
+++ src/main/java/cl/sprint_rocket_ai/ms_ai_engine/infrastructure/config/semantic_cache/SemanticCacheAdvisor.java
@@ -16,47 +16,39 @@ import java.util.Optional;
 @Component
 public class SemanticCacheAdvisor {
 
+    private static final Logger log = LoggerFactory.getLogger(SemanticCacheAdvisor.class);
+
+    private final VectorStore cacheStore;
+
+    @Value("${semantic.cache.similarity-threshold}")
+    private double similarityThreshold;
+
+    public SemanticCacheAdvisor(@Qualifier("redisCacheVectorStore") VectorStore cacheStore) {
+        this.cacheStore = cacheStore;
+    }
+
     public Optional<String> findInCache(String question) {
-        return Optional.empty();
+        log.info("Buscando existencia en caché");
+        List<Document> hits = cacheStore.similaritySearch(
+                SearchRequest.builder()
+                        .query(question)
+                        .topK(1)
+                        .similarityThreshold(similarityThreshold)
+                        .build()
+        );
+        log.info("Fin de búsqueda, registro en caché: {}", hits.size());
+        return hits.stream()
+                .findFirst()
+                .map(doc -> (String) doc.getMetadata().get("answer"));
     }
 
     public void saveToCache(String question, String answer) {
-        // no-op
+        log.info("Iniciando guardado en semantic caché");
+        Document documentToSave = Document.builder()
+                .text(question)
+                .metadata(Map.of("answer", answer))
+                .build();
+        cacheStore.add(List.of(documentToSave));
+        log.info("Caché Guardado para la pregunta: {}", question);
     }
-
-//    private static final Logger log = LoggerFactory.getLogger(SemanticCacheAdvisor.class);
-//
-//    private final VectorStore cacheStore;
-//
-//    @Value("${semantic.cache.similarity-threshold}")
-//    private double similarityThreshold;
-//
-//    public SemanticCacheAdvisor(@Qualifier("redisCacheVectorStore") VectorStore cacheStore) {
-//        this.cacheStore = cacheStore;
-//    }
-//
-//    public Optional<String> findInCache(String question) {
-//        log.info("Buscando existencia en caché");
-//        List<Document> hits = cacheStore.similaritySearch(
-//                SearchRequest.builder()
-//                        .query(question)
-//                        .topK(1)
-//                        .similarityThreshold(similarityThreshold)
-//                        .build()
-//        );
-//        log.info("Fin de búsqueda, registro en caché: {}",hits.size());
-//        return hits.stream()
-//                .findFirst()
-//                .map(doc -> (String) doc.getMetadata().get("answer"));
-//    }
-//
-//    public void saveToCache(String question, String answer) {
-//        log.info("Iniciando guardado en semantic caché");
-//        Document documentToSave = Document.builder()
-//                .text(question)
-//                .metadata(Map.of("answer", answer))
-//                .build();
-//        cacheStore.add(List.of(documentToSave));
-//        log.info("Caché Guardado para la pregunta: {}", question);
-//    }
 }
