895a90058c829d5529c4f6a3cb6ec723a30bbfc3
matias_valdes <matias.valdes@bicevida.cl>
2026-07-05 23:04:49 -0400

    feat(ai-engine-sls)

--- src/main/java/cl/sprint_rocket_ai/ms_ai_engine/MsAiEngineApplication.java
+++ src/main/java/cl/sprint_rocket_ai/ms_ai_engine/MsAiEngineApplication.java
@@ -1,13 +1,13 @@
 package cl.sprint_rocket_ai.ms_ai_engine;
 
-//import org.springframework.ai.model.google.genai.autoconfigure.chat.GoogleGenAiChatAutoConfiguration;
+import org.springframework.ai.model.google.genai.autoconfigure.chat.GoogleGenAiChatAutoConfiguration;
 import org.springframework.ai.vectorstore.redis.autoconfigure.RedisVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude = {
         RedisVectorStoreAutoConfiguration.class,
-        //GoogleGenAiChatAutoConfiguration.class
+        GoogleGenAiChatAutoConfiguration.class
 })
 public class MsAiEngineApplication {
 
